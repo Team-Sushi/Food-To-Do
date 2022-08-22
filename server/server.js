9b9957d37e637a0ee32e426af89050b4b0d37def
PixieFoxy <imanuel_david88@yahoo.co.id>
2022-08-23 00:30:01 +1000

    Initial setup for the project

---
+++ server/server.js
@@ -0,0 +1,13 @@
+require('dotenv').config({path: './config.env'});
+
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
