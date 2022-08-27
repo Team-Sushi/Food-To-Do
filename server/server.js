c800fd5037f74d735038f7a3406a3eb658f89c76
PixieFoxy <imanuel_david88@yahoo.co.id>
2022-08-27 23:55:18 +1000

    added database connection functionality

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
-require('dotenv').config({path: './config.env'});
+require('dotenv').config();
+require('./database');
 
 const express = require('express');
 const app = express();
@@ -8,6 +9,9 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    res.send('<h1>Team Sushi\'s grocery app is coming soon!</h1>');
+})
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 })
