e281870b1e9cd407da1e4a05e026b406c8355296
natnutella <81858123+natnutella@users.noreply.github.com>
2022-09-13 01:08:38 +1000

    added a favicon and a logo and also refactored out loginbox as a component

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -1,39 +1,10 @@
 import React from 'react';
+import LoginBox from '../components/loginBox';
 import './loginSignUp.css';
 
 function login() {
   return (
-    <div className='login'>
-      <div class="login-signup-button">
-        <button>
-          <a href="/signup">Sign Up</a>
-          <span> | </span>
-          <a href="/login">Login</a>
-        </button>
-      </div>
-      <form action="login" method="POST" className="login-form">
-        <span>
-          <label for="email">
-            <i class="material-symbols-outlined">mail</i>Emails
-          </label>
-          <input className="login-fields" type="email" name="email" />
-        </span>
-
-        <span>
-          <label for="password">
-            <i class="material-symbols-outlined">lock</i>Password
-          </label>
-          <input className="login-fields" type="password" name="password" />
-        </span>
-
-        <span className="keep-signedin">
-          <label>Keep me signed in</label>
-          <input type='checkbox' name="signed-in" value="required" />
-        </span>
-
-        <input id="submit-button" type="submit" value="Login" />
-      </form>
-    </div>
+    <LoginBox />
   );
 }
 
