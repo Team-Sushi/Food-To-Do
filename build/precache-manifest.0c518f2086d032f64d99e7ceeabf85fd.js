bed8e2484a4a6b0e298f262315f628a9cbc1a29f
Nattasha Techathawiekul <nattasha@Nattashas-MacBook-Air.local>
2022-09-28 01:06:46 +1000

    modified dependancies and rebuilt

---
+++ build/precache-manifest.0c518f2086d032f64d99e7ceeabf85fd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9856e719218f3d4cf6fe",
+    "url": "/static/css/main.ccb556a9.chunk.css"
+  },
+  {
+    "revision": "9856e719218f3d4cf6fe",
+    "url": "/static/js/main.9856e719.chunk.js"
+  },
+  {
+    "revision": "836e4380fe3f7816a983",
+    "url": "/static/js/1.836e4380.chunk.js"
+  },
+  {
+    "revision": "bc650b2ab4a1ec328eef",
+    "url": "/static/js/2.bc650b2a.chunk.js"
+  },
+  {
+    "revision": "d2051469dccdba7ca17d",
+    "url": "/static/js/runtime~main.d2051469.js"
+  },
+  {
+    "revision": "8fcf6ee1bef2b1d11d4f04a5cc79b51c",
+    "url": "/static/media/foodToDoLogo.8fcf6ee1.png"
+  },
+  {
+    "revision": "1284d968ef395b9fd6188f917e346979",
+    "url": "/static/media/background1.1284d968.jpg"
+  },
+  {
+    "revision": "594b78c3399a6ab608aecf6df8e2d065",
+    "url": "/index.html"
+  }
+];
