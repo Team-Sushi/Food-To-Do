556952f9773de1c54083b62bbfb6cafdd5502b1e
Nattasha Techathawiekul <81858123+natnutella@users.noreply.github.com>
2022-10-04 04:21:58 +1100

    removed server files from app

--- server/controllers/users/index.js
+++
@@ -1,7 +0,0 @@
-const { findUserByEmail } = require('./findUserByEmail.js');
-const { registerUser } = require('./registerUser.js');
-
-module.exports = {
-    findUserByEmail,
-    registerUser
-};
