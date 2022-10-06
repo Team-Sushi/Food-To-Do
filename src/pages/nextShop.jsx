058fd3512cc68cb5b7d44e1aa8d3a24166dc819c
jaynwong <jaynwong@gmail.com>
2022-10-06 16:36:54 +1100

    deleted redundant logo files

--- src/pages/nextShop.jsx
+++ src/pages/nextShop.jsx
@@ -1,32 +1,17 @@
 import React from 'react';
 import "./nextShop.css";
-import logo from "./Logo.png";
-import menu from "./menu.png";
-import search from "./search.png";
 import skipback from "./skip-back.png";
 import Navbar from '../components/navbar';
 
 const NextShop = () => {
   return (
-    // <div>
-    //   <Navbar />
       <div className="next-shopping-list-desktop">
-        {/* <div className="header-div">
-          <div className="icon-buttons-div">
-            <a href="https://foodtodo.herokuapp.com/">
-              <img className="master-logo-icon" alt="" src={logo} />
-            </a>
-            <div className="search-and-menu">
-              <img className="search-icon" alt="" src={search} />
-              <img className="menu-icon" alt="" src={menu} />
-            </div>
-          </div>
-        </div> */}
         <Navbar />
         <div className="next-shopping-list-title-group">
           <img className="skip-back-icon" alt="" src={skipback} />
           <div className="next-shopping-list">Next Shopping List</div>
         </div>
+
         <div className="content-div">
           <div className="demo-items-group">
             <div className="items-row-group">
@@ -87,13 +72,13 @@ const NextShop = () => {
             </div>
           </div>
         </div>
+
         <div className="made-with-by-teamsushi">
           <span>{`Made with `}</span>
           <span className="span">♥</span>
           <span> by TeamSushi</span>
         </div>
       </div>
-    // </div>
   );
 };
 
