e9f79b6ff028a365606c585c07aca3f128fbb5d7
jaynwong <jaynwong@gmail.com>
2022-10-07 23:24:18 +1100

    added new files for next shopping list's content display

---
+++ src/components/nextShopContentDisplay.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import './node_modules/react-grid-layout/css/styles.css';
+import './node_modules/react-resizable/css/styles.css';
+import { Responsive as ResponsiveGridLayout } from "react-grid-layout";
+
+
+class nextShopContentDisplay extends Component {
+
+    demoArray = { items: [
+        {id: 1, title: 'item #1', imageURL:'./unavailable.png'},
+        {id: 2, title: 'item #2', imageURL:'./unavailable.png'}
+        {id: 3, title: 'item #3', imageURL:'./unavailable.png'}
+        {id: 4, title: 'item #4', imageURL:'./unavailable.png'}
+        {id: 5, title: 'item #5', imageURL:'./unavailable.png'}
+        ]
+    }
+    
+}
