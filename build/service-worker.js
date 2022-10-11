8ff3d72f721bf133ede575de534d1623646f5bff
Jason <jasonnichola@student.unimelb.edu.au>
2022-10-11 15:11:12 +1100

    Created all items category

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0ecc918121d70ffe3ac8c1c65857a92c.js"
+  "/precache-manifest.2147cd359ffe91a9558a18a9dee0b489.js"
 );
 
 workbox.clientsClaim();
