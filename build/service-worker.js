c04feae936c0e66cef77750c4990185d49d63642
Nattasha Techathawiekul <81858123+natnutella@users.noreply.github.com>
2022-10-16 23:29:39 +1100

    implemented protected routes for item pages

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4b862318a81eb5b73bb69f63e786095c.js"
+  "/precache-manifest.dc7291b4c44cd824641be4c0d1fdc9e6.js"
 );
 
 workbox.clientsClaim();
